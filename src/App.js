6662a17560dc3885c2e302e535465fde96520274
Beatriz Januário <bibi_januario@hotmail.com>
2020-06-27 12:50:11 -0300

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+
 import './global.css'
 
 function App() {
